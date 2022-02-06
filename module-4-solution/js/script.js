32f8fee9958ac62861dd4735a673a37a279c8219
Tr.ds <trushildhokiya26@gmail.com>
2022-02-06 15:45:53 +0530

    updated

---
+++ module-4-solution/js/script.js
@@ -0,0 +1,18 @@
+(function () {
+  
+    var names = ["King", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  
+ 
+    for (var i = 0; i < names.length; i++) {
+  
+      
+       var firstLetter = names[i].charAt(0).toLowerCase();
+  
+     
+      if (firstLetter === 'j') {
+         byeSpeaker.speak(names[i]);
+      } else {
+        helloSpeaker.speak(names[i]);
+      }
+    }
+  })();
